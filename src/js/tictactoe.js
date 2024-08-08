5dab76dcd6d1ac3ead496ec780fb0ed0e59a6784
wendybarrantes <wenbarrantes06@gmail.com>
2024-08-08 16:03:59 -0600

    Logica y estilos terminados

--- src/js/tictactoe.js
+++ src/js/tictactoe.js
@@ -9,9 +9,17 @@ let casilla8 = document.getElementById("casilla8")
 let casilla9 = document.getElementById("casilla9")
 let casillas=[casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8,casilla9]
 let btnReinicio = document.getElementById("btnReinicio")
-
-let x = `<img src = ${"https://img.icons8.com/?size=100&id=110870&format=png&color=000000"} alt="X">`
-let o = `<img src = ${"https://img.icons8.com/?size=100&id=110864&format=png&color=000000"} alt="O">`
+// casillas[0].innerHTML="0"
+// casillas[1].innerHTML="1"
+// casillas[2].innerHTML="2"
+// casillas[3].innerHTML="3"
+// casillas[4].innerHTML="4"
+// casillas[5].innerHTML="5"
+// casillas[6].innerHTML="6"
+// casillas[7].innerHTML="7"
+// casillas[8].innerHTML="8"
+let x = `<img src = ${"https://emojigraph.org/media/htc/cross-mark_274c.png"} alt="X">`
+let o = `<img src = ${"https://emojigraph.org/media/htc/hollow-red-circle_2b55.png"} alt="O">`
 let juegoFinalizado = false
 let contadorMovimientos = 0
 
@@ -35,6 +43,7 @@ function movimientoHumano() {
         let casillasVacias = casillas.filter((casilla)=> casilla.innerHTML == "");
         let aleatorio = Math.floor(Math.random() * casillasVacias.length);
         casillasVacias[aleatorio].innerHTML = o
+        validarGanador()
      },"500");
      
     }
@@ -42,7 +51,7 @@ function movimientoHumano() {
     function validarGanador() {
         const posicionesGanadoras = [
             [0,1,2],[3,4,5],[6,7,8], //  FILAS
-            [0,3,6],[3,4,7],[2,5,8], // COLUMNAS
+            [0,3,6],[1,4,7],[2,5,8], // COLUMNAS
             [0,4,8],[2,4,6] // DIAGONALES
         ]
         posicionesGanadoras.forEach((posicion)=>{
@@ -55,7 +64,7 @@ function movimientoHumano() {
                     marcadorDelJugador++
                     localStorage.setItem("jugador",marcadorDelJugador)
                     alert("VICTORIA JUGADOR")
-                }else{
+                }else if(casillas[posicion1].innerHTML.includes("O")){
                     marcadorDeCompu++
                     localStorage.setItem("compu",marcadorDeCompu)
                     alert("VICTORIA COMPU")
@@ -83,18 +92,18 @@ function movimientoHumano() {
         compu.innerHTML=marcadorDeCompu
     
     }
-    function reiniciarJuego(){
-        casillas.forEach(casilla=>{
-            casilla.innerHTML=""
-        })
-        contadorMovimientos = 0
-        juegoFinalizado = false
-    }
-    btnReinicio.addEventListener("click",reiniciarJuego)
-    
-    
-    marcador()
-    movimientoHumano()
+function reiniciarJuego(){
+    casillas.forEach(casilla=>{
+        casilla.innerHTML=""
+    })
+    contadorMovimientos = 0
+    juegoFinalizado = false
+}
+btnReinicio.addEventListener("click",reiniciarJuego)
+
+
+marcador()
+movimientoHumano()
 
 
 
